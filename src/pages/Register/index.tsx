bb2e0ecf3bd6d02e2559b272a69c6209f69d2da4
Josué Cesar <mendonca.josue@gmail.com>
2020-06-16 17:21:59 -0300

    att

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
 import logo from '../../assets/globe.png';
 import { Nav, GoBack, FormSection } from './styles';
@@ -9,7 +9,41 @@ import Footer from '../../components/Footer';
 import { FaArrowLeft } from 'react-icons/fa';
 import { FiLogIn } from 'react-icons/fi';
 
+interface IData {
+  name: string;
+  email: string;
+  password: string;
+  cPassword: string;
+}
+
 const Login: React.FC = () => {
+
+  const [passwordEquals, setPasswordEquals] = useState(false);
+  const [formData, setFormData] = useState<IData>({
+    name: '',
+    email: '',
+    password: '',
+    cPassword: '',
+  });
+
+  const handleDataInput = (event: ChangeEvent<HTMLInputElement>) => {
+    if(event.target.name === 'password' || event.target.name === 'cPassword') {
+      handleConfirmPassword();
+    }
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleConfirmPassword = () => {
+    formData.password === formData.cPassword ?
+    setPasswordEquals(true) : setPasswordEquals(false);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const { name, email, password, cPassword } = formData;
+  };
+
   return (
     <>
       <Nav>
@@ -31,22 +65,42 @@ const Login: React.FC = () => {
             <div className="fieldGroup">
               <div className="fieldItem">
                 <label htmlFor="name">Nome</label>
-                <input type="text" name="name" />
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleDataInput}
+                />
               </div>
 
               <div className="fieldItem">
                 <label htmlFor="name">E-mail</label>
-                <input type="text" name="email"/>
+                <input
+                  type="text"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleDataInput}
+                />
               </div>
 
               <div className="fieldItem">
                 <label htmlFor="name">Senha</label>
-                <input type="password" name="password"/>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleDataInput}
+                />
               </div>
 
               <div className="fieldItem">
                 <label htmlFor="name">Confirme sua senha</label>
-                <input type="password" name="cpassword"/>
+                <input
+                  type="password"
+                  name="cpassword"
+                  value={formData.cPassword}
+                  onChange={handleDataInput}
+                />
               </div>
 
               <button type="submit">Enviar</button>
