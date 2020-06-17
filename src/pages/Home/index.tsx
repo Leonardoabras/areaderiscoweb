cdbe9cc657098b25c354084eff504c6205cd81a1
Josué Cesar <mendonca.josue@gmail.com>
2020-06-17 12:07:20 -0300

    merge resolvido

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../hooks/AuthContext';
 
 import { Map, TileLayer, Marker } from 'react-leaflet';
